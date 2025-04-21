90a1525c8d5002f18982ff576c4345ac42e7da9e
GitCoder <gitcoder.net@gmail.com>
2025-04-21 18:44:00 +0300

    changed class name

--- src/main/java/ru/gitcoder/telegram/api/configuration/UpdateConfig.java
+++ src/main/java/ru/gitcoder/telegram/api/configuration/UpdateConfig.java
@@ -2,11 +2,11 @@ package ru.gitcoder.telegram.api.configuration;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import ru.gitcoder.telegram.api.update.UpdateMethod;
+import ru.gitcoder.telegram.api.update.UpdateType;
 import ru.gitcoder.telegram.api.utility.NullableObject;
 
 @Getter
 @RequiredArgsConstructor
-public abstract class UpdateMethodConfig implements NullableObject {
-    private final UpdateMethod method;
+public abstract class UpdateConfig implements NullableObject {
+    private final UpdateType type;
 }
