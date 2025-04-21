ee6ea4d0829055208a1d5944c18da89bd1933ab7
GitCoder <gitcoder.net@gmail.com>
2025-04-21 18:42:30 +0300

    field name pollingTimeout to timeout

--- src/main/java/ru/gitcoder/telegram/api/configuration/LongPollingConfig.java
+++ src/main/java/ru/gitcoder/telegram/api/configuration/LongPollingConfig.java
@@ -2,21 +2,21 @@ package ru.gitcoder.telegram.api.configuration;
 
 import lombok.Builder;
 import lombok.Getter;
-import ru.gitcoder.telegram.api.update.UpdateMethod;
+import ru.gitcoder.telegram.api.update.UpdateType;
 
 @Getter
 @Builder
-public class LongPollingConfig extends UpdateMethodConfig {
+public class LongPollingConfig extends UpdateConfig {
     private static final int DEFAULT_LONG_POLLING_TIMEOUT = 20;
     
-    private final int pollingTimeout;
+    private final int timeout;
 
-    public LongPollingConfig(int pollingTimeout) {
-        super(UpdateMethod.LONG_POLLING);
-        if (pollingTimeout < 0) {
+    public LongPollingConfig(int timeout) {
+        super(UpdateType.LONG_POLLING);
+        if (timeout < 0) {
             throw new IllegalArgumentException("Timeout must be non-negative");
         }
-        this.pollingTimeout = pollingTimeout;
+        this.timeout = timeout;
     }
 
     public LongPollingConfig() {
