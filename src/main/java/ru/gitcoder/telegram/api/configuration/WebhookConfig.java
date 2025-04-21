432510b8d2e6155a7e24f559297e251689bd0895
GitCoder <gitcoder.net@gmail.com>
2025-04-21 18:44:31 +0300

    changed use super

--- src/main/java/ru/gitcoder/telegram/api/configuration/WebhookConfig.java
+++ src/main/java/ru/gitcoder/telegram/api/configuration/WebhookConfig.java
@@ -2,15 +2,15 @@ package ru.gitcoder.telegram.api.configuration;
 
 import lombok.Builder;
 import lombok.Getter;
-import ru.gitcoder.telegram.api.update.UpdateMethod;
+import ru.gitcoder.telegram.api.update.UpdateType;
 
 @Getter
 @Builder
-public class WebhookConfig extends UpdateMethodConfig {
+public class WebhookConfig extends UpdateConfig {
     private final String webhookUrl;
 
     public WebhookConfig(String webhookUrl) {
-        super(UpdateMethod.WEBHOOK);
+        super(UpdateType.WEBHOOK);
         if (webhookUrl == null || webhookUrl.trim().isEmpty()) {
             throw new IllegalArgumentException("Webhook URL must not be null or empty");
         }
